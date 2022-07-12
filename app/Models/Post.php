fec25051ad9904082d2231f043acca5fc476d34b
hellsontime <roma67177@gmail.com>
2022-07-12 18:25:52 +0300

    Post model boot method implemented

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,6 +11,23 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Post extends Model
 {
     use HasFactory;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        self::creating(function (Post $post): void {
+            if ($post->author_id === null) {
+                $user = auth()->user();
+                if ($user === null) {
+                    throw new AuthenticationException();
+                }
+                assert($user instanceof User);
+
+                $post->author_id = $user->id;
+            }
+        });
+    }
 
     public function author(): BelongsTo
     {
